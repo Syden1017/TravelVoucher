cb44833599e66d1eaf1b357f11bc8b0a3a93dfcb
Syden1017 <sasha.shustov.05@mail.ru>
2023-10-23 17:48:57 +0300

    Add changes to C# code at login page, add changes to C# code at top up balance page(23.10.2023)

--- TravelVoucher/Pages/MainPages/LoginPage.xaml.cs
+++ TravelVoucher/Pages/MainPages/LoginPage.xaml.cs
@@ -25,6 +25,12 @@ namespace TravelVoucher.Pages
 
         private void txtBoxLogin_LostFocus(object sender, RoutedEventArgs e)
         {
+            Color colorWhite = Color.FromRgb(255, 242, 242);
+            Brush brushWhite = new SolidColorBrush(colorWhite);
+
+            Color colorBlack = Color.FromRgb(0, 0, 0);
+            Brush brushBlack = new SolidColorBrush(colorBlack);
+
             if (txtBoxLogin.Text == "")
             {
                 txtBoxLogin.Background = Brushes.LightCoral;
@@ -32,12 +38,6 @@ namespace TravelVoucher.Pages
             }
             else
             {
-                Color colorWhite = Color.FromRgb(255, 242, 242);
-                Brush brushWhite = new SolidColorBrush(colorWhite);
-
-                Color colorBlack = Color.FromRgb(0, 0, 0);
-                Brush brushBlack = new SolidColorBrush(colorBlack);
-
                 txtBoxLogin.Background = brushWhite;
                 txtBoxLogin.BorderBrush = brushBlack;
             }
@@ -45,6 +45,12 @@ namespace TravelVoucher.Pages
 
         private void passBox_LostFocus(object sender, RoutedEventArgs e)
         {
+            Color colorWhite = Color.FromRgb(255, 242, 242);
+            Brush brushWhite = new SolidColorBrush(colorWhite);
+
+            Color colorBlack = Color.FromRgb(0, 0, 0);
+            Brush brushBlack = new SolidColorBrush(colorBlack);
+
             if (passBox.Password == "")
             {
                 passBox.Background = Brushes.LightCoral;
@@ -52,12 +58,6 @@ namespace TravelVoucher.Pages
             }
             else
             {
-                Color colorWhite = Color.FromRgb(255, 242, 242);
-                Brush brushWhite = new SolidColorBrush(colorWhite);
-
-                Color colorBlack = Color.FromRgb(0, 0, 0);
-                Brush brushBlack = new SolidColorBrush(colorBlack);
-
                 passBox.Background = brushWhite;
                 passBox.BorderBrush = brushBlack;
             }
@@ -71,74 +71,126 @@ namespace TravelVoucher.Pages
             Color colorBlack = Color.FromRgb(0, 0, 0);
             Brush brushBlack = new SolidColorBrush(colorBlack);
 
-            if (txtBoxLogin.Text.Length > 0)
+            if (txtBoxLogin.Text.Length > 0
+                && passBox.Password.Length > 0)
             {
-                if (passBox.Password.Length > 0)
+                if (txtBoxLogin.Text == "User"
+                    && passBox.Password == "12345")
                 {
-                    if (txtBoxLogin.Text == "User"
-                        && passBox.Password == "12345")
-                    {
-                        Navigation.frmObj.Navigate(new HomePage());
-                    }
-                    else
-                    {
-                        MessageBox.Show(
-                            "Неправильный логин или пароль",
-                            "Ошибка авторизации",
-                            MessageBoxButton.OK,
-                            MessageBoxImage.Error
-                            );
-
-                        if (txtBoxLogin.Text != "User")
-                        {
-                            txtBoxLogin.Background = Brushes.LightCoral;
-                            txtBoxLogin.BorderBrush = Brushes.Red;
-                        }
-                        else
-                        {
-                            txtBoxLogin.Background = brushWhite;
-                            txtBoxLogin.BorderBrush = brushBlack;
-                        }
-
-                        if (passBox.Password != "12345")
-                        {
-                            passBox.Background = Brushes.LightCoral;
-                            passBox.BorderBrush = Brushes.Red;
-                        }
-                        else
-                        {
-                            passBox.Background = brushWhite;
-                            passBox.BorderBrush = brushBlack;
-                        }
-                    }
+                    Navigation.frmObj.Navigate(new HomePage());
                 }
                 else
                 {
                     MessageBox.Show(
-                        "Введите пароль!",
+                        "Неправильный логин или пароль",
                         "Ошибка авторизации",
                         MessageBoxButton.OK,
                         MessageBoxImage.Error
                         );
 
-                    passBox.Background = Brushes.LightCoral;
-                    passBox.BorderBrush = Brushes.Red;
+                    if (txtBoxLogin.Text != "User")
+                    {
+                        txtBoxLogin.Background = Brushes.LightCoral;
+                        txtBoxLogin.BorderBrush = Brushes.Red;
+                    }
+                    else
+                    {
+                        txtBoxLogin.Background = brushWhite;
+                        txtBoxLogin.BorderBrush = brushBlack;
+                    }
+
+                    if (passBox.Password != "12345")
+                    {
+                        passBox.Background = Brushes.LightCoral;
+                        passBox.BorderBrush = Brushes.Red;
+                    }
+                    else
+                    {
+                        passBox.Background = brushWhite;
+                        passBox.BorderBrush = brushBlack;
+                    }
                 }
             }
             else
             {
-                MessageBox.Show(
-                    "Введите логин и пароль!",
-                    "Ошибка авторизации",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Error
-                    );
+                if (txtBoxLogin.Text == ""
+                    && passBox.Password == "")
+                {
+                    MessageBox.Show(
+                        "Введите логин и пароль!",
+                        "Ошибка авторизации",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error
+                        );
 
-                txtBoxLogin.Background = Brushes.LightCoral;
-                txtBoxLogin.BorderBrush = Brushes.Red;
+                    txtBoxLogin.Background = Brushes.LightCoral;
+                    txtBoxLogin.BorderBrush = Brushes.Red;
 
-                passBox.Background = Brushes.LightCoral;
-                passBox.BorderBrush = Brushes.Red;
+                    passBox.Background = Brushes.LightCoral;
+                    passBox.BorderBrush = Brushes.Red;
+                }
+                else if (txtBoxLogin.Text.Length > 0 
+                         && passBox.Password == "")
+                {
+                    if (txtBoxLogin.Text == "User")
+                    {
+                        txtBoxLogin.Background = brushWhite;
+                        txtBoxLogin.BorderBrush = brushBlack;
+
+                        MessageBox.Show(
+                            "Введите пароль!",
+                            "Ошибка авторизации",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error
+                            );
+                    }
+                    else
+                    {
+                        MessageBox.Show(
+                            "Введите пароль и корректное имя пользователя!",
+                            "Ошибка авторизации",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error
+                            );
+
+                        txtBoxLogin.Background = Brushes.LightCoral;
+                        txtBoxLogin.BorderBrush = Brushes.Red;
+                    }
+
+                    passBox.Background = Brushes.LightCoral;
+                    passBox.BorderBrush = Brushes.Red;
+                }
+                else if (txtBoxLogin.Text == ""
+                         && passBox.Password.Length > 0)
+                {
+                    txtBoxLogin.Background = Brushes.LightCoral;
+                    txtBoxLogin.BorderBrush = Brushes.Red;
+
+                    if (passBox.Password == "12345")
+                    {
+                        passBox.Background = brushWhite;
+                        passBox.BorderBrush = brushBlack;
+
+                        MessageBox.Show(
+                            "Введите логин!",
+                            "Ошибка авторизации",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error
+                            );
+                    }
+                    else
+                    {
+                        MessageBox.Show(
+                            "Введите логин и корректный пароль!",
+                            "Ошибка авторизации",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error
+                            );
+
+                        passBox.Background = Brushes.LightCoral;
+                        passBox.BorderBrush = Brushes.Red;
+                    }
+                }
             }
         }
     }
--- TravelVoucher/Windows/TopUpBalance.xaml.cs
+++ TravelVoucher/Windows/TopUpBalance.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace TravelVoucher.Windows
 {
@@ -35,14 +36,148 @@ namespace TravelVoucher.Windows
 
         private void btnTransaction_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(
-                "Транзакция прошла успешно",
-                "Выполнения пополнения",
-                MessageBoxButton.OK,
-                MessageBoxImage.Information
-                );
-            
-            this.Close();
+            Color colorWhite = Color.FromRgb(255, 242, 242);
+            Brush brushWhite = new SolidColorBrush(colorWhite);
+
+            Color colorBlack = Color.FromRgb(0, 0, 0);
+            Brush brushBlack = new SolidColorBrush(colorBlack);
+
+            if (txtBoxCardNumber.Text.Length > 0
+                && txtBoxMonth.Text.Length > 0
+                && txtBoxYear.Text.Length > 0
+                && txtBoxCodeCVC.Text.Length > 0
+                && txtBoxReplenishmentAmount.Text.Length > 0)
+            {
+                MessageBox.Show(
+                    "Транзакция прошла успешно",
+                    "Выполнения пополнения",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information
+                    );
+
+                this.Close();
+                
+            }
+            else
+            {
+                if (txtBoxCardNumber.Text == ""
+                    && txtBoxMonth.Text == ""
+                    && txtBoxYear.Text == ""
+                    && txtBoxCodeCVC.Text == ""
+                    && txtBoxReplenishmentAmount.Text == "")
+                {
+                    MessageBox.Show(
+                        "Введите номер, срок действия, " +
+                        "код на обратной стороне карты " +
+                        "и сумму пополнения",
+                        "Ошибка пополнения",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                        );
+
+                    txtBoxCardNumber.Background = Brushes.LightCoral;
+                    txtBoxCardNumber.BorderBrush = Brushes.Red;
+
+                    txtBoxMonth.Background = Brushes.LightCoral;
+                    txtBoxMonth.BorderBrush = Brushes.Red;
+
+                    txtBoxYear.Background = Brushes.LightCoral;
+                    txtBoxYear.BorderBrush = Brushes.Red;
+
+                    txtBoxCodeCVC.Background = Brushes.LightCoral;
+                    txtBoxCodeCVC.BorderBrush = Brushes.Red;
+
+                    txtBoxReplenishmentAmount.Background = Brushes.LightCoral;
+                    txtBoxReplenishmentAmount.BorderBrush = Brushes.Red;
+                }
+                else if (txtBoxCardNumber.Text.Length > 0
+                         && txtBoxMonth.Text == ""
+                         && txtBoxYear.Text == ""
+                         && txtBoxCodeCVC.Text == ""
+                         && txtBoxReplenishmentAmount.Text == "")
+                {
+                    MessageBox.Show(
+                        "Введите срок действия, " +
+                        "код на обратной стороне карты " +
+                        "и сумму пополнения",
+                        "Ошибка пополнения",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                        );
+
+                    txtBoxCardNumber.Background = brushWhite;
+                    txtBoxCardNumber.BorderBrush = brushBlack;
+
+                    txtBoxMonth.Background = Brushes.LightCoral;
+                    txtBoxMonth.BorderBrush = Brushes.Red;
+
+                    txtBoxYear.Background = Brushes.LightCoral;
+                    txtBoxYear.BorderBrush = Brushes.Red;
+
+                    txtBoxCodeCVC.Background = Brushes.LightCoral;
+                    txtBoxCodeCVC.BorderBrush = Brushes.Red;
+
+                    txtBoxReplenishmentAmount.Background = Brushes.LightCoral;
+                    txtBoxReplenishmentAmount.BorderBrush = Brushes.Red;
+                }
+                else if (txtBoxCardNumber.Text.Length > 0
+                         && txtBoxMonth.Text.Length > 0
+                         && txtBoxYear.Text.Length > 0
+                         && txtBoxCodeCVC.Text == ""
+                         && txtBoxReplenishmentAmount.Text == "")
+                {
+                    MessageBox.Show(
+                        "Введите код на обратной стороне карты " +
+                        "и сумму пополнения",
+                        "Ошибка пополнения",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                        );
+
+                    txtBoxCardNumber.Background = brushWhite;
+                    txtBoxCardNumber.BorderBrush = brushBlack;
+
+                    txtBoxMonth.Background = brushWhite;
+                    txtBoxMonth.BorderBrush = brushBlack;
+
+                    txtBoxYear.Background = brushWhite;
+                    txtBoxYear.BorderBrush = brushBlack;
+
+                    txtBoxCodeCVC.Background = Brushes.LightCoral;
+                    txtBoxCodeCVC.BorderBrush = Brushes.Red;
+
+                    txtBoxReplenishmentAmount.Background = Brushes.LightCoral;
+                    txtBoxReplenishmentAmount.BorderBrush = Brushes.Red;
+                }
+                else if (txtBoxCardNumber.Text.Length > 0
+                         && txtBoxMonth.Text.Length > 0
+                         && txtBoxYear.Text.Length > 0
+                         && txtBoxCodeCVC.Text .Length > 0
+                         && txtBoxReplenishmentAmount.Text == "")
+                {
+                    MessageBox.Show(
+                        "Введите сумму пополнения",
+                        "Ошибка пополнения",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning
+                        );
+
+                    txtBoxCardNumber.Background = brushWhite;
+                    txtBoxCardNumber.BorderBrush = brushBlack;
+
+                    txtBoxMonth.Background = brushWhite;
+                    txtBoxMonth.BorderBrush = brushBlack;
+
+                    txtBoxYear.Background = brushWhite;
+                    txtBoxYear.BorderBrush = brushBlack;
+
+                    txtBoxCodeCVC.Background = brushWhite;
+                    txtBoxCodeCVC.BorderBrush = brushBlack;
+
+                    txtBoxReplenishmentAmount.Background = Brushes.LightCoral;
+                    txtBoxReplenishmentAmount.BorderBrush = Brushes.Red;
+                }
+            }
         }
     }
 }
