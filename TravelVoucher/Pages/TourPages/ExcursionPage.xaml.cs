f2966ea1c8830d7dae2602037b0a9a9ce71f5df8
исп-32 <-32@LAB30-05>
2023-12-01 09:03:44 +0300

    frjionerfonjifrseffklsefojkbnergifhdrjfbkfn

--- TravelVoucher/Pages/TourPages/ExcursionPage.xaml.cs
+++ TravelVoucher/Pages/TourPages/ExcursionPage.xaml.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using Humanizer;
+using System;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Imaging;
 using TravelVoucher.Tools;
 using WpfApp1.Tools;
-using Xceed.Wpf.Toolkit;
 
 namespace TravelVoucher.Pages.TourPages
 {
@@ -37,15 +35,76 @@ namespace TravelVoucher.Pages.TourPages
         private void btnFindTickets_Click(object sender, RoutedEventArgs e)
         {
             DateTime selectedDate = datePickerExcursion.SelectedDate ?? DateTime.Now;
+            string selectedCity = cBoxTo.SelectedItem.ToString();
+            string name = GetExcursionName(selectedCity);
+            double calculatedPrice = CalculatePrice();
+            string img = GetImagePath(selectedCity);
+
+            ExcursionItem newItem = new ExcursionItem
+            {
+                Name = name,
+                Date = selectedDate,
+                Price = calculatedPrice,
+                CityImage = img
+            };
+
+            lViewFoundExcursions.Items.Add(newItem);
         }
 
-        private void cBoxTo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void btnBack_Click(object sender, RoutedEventArgs e)
         {
-            String selectedItem = (String)cBoxTo.SelectedItem;
+            Navigation.frmObj.GoBack();
+        }
+
+        #region Методы
+        private double CalculatePrice()
+        {
+            double price = 0.00;
+
+            string selectedItem = (String)cBoxExcursionType.SelectedItem;
 
             if (selectedItem != null)
             {
-                string selectedCity = selectedItem.ToString();
+                string selectedExcursionType = selectedItem.ToString();
+
+                switch(selectedExcursionType) 
+                {
+                    case "обзорная":
+                        price = 1800.00 * Convert.ToInt32(upDownTourist.Text);
+                        break;
+
+                    case "историческая":
+                        price = 2400.00 * Convert.ToInt32(upDownTourist.Text);
+                        break;
+
+                    case "городская":
+                        price = 3000.00 * Convert.ToInt32(upDownTourist.Text);
+                        break;
+                    
+                    case "загородная":
+                        price = 3400.00 * Convert.ToInt32(upDownTourist.Text);
+                        break;
+
+                    case "музейная":
+                        price = 2000.00 * Convert.ToInt32(upDownTourist.Text);
+                        break;
+
+                    case "учебная":
+                        price = 800.00 * Convert.ToInt32(upDownTourist.Text);
+                        break;
+                }
+            }
+
+            return price;
+        }
+
+        private string GetImagePath(string city)
+        {
+            string imagePath = "";
+
+            if (city != null)
+            {
+                string selectedCity = city.ToString();
 
                 switch (selectedCity)
                 {
@@ -57,50 +116,15 @@ namespace TravelVoucher.Pages.TourPages
                         break;
                 }
             }
+
+            return imagePath;
         }
 
-        private void btnBack_Click(object sender, RoutedEventArgs e)
+        private string GetExcursionName(string selectedCity)
         {
-            Navigation.frmObj.GoBack();
+            string excursionName = "Экскурсия по " + selectedCity.Humanize(LetterCasing.Title);
+            return excursionName;
         }
-
-        private void CalculatePrice()
-        {
-            int price = 0;
-
-            string selectedItem = (String)cBoxExcursionType.SelectedItem;
-
-            if (selectedItem != null)
-            {
-                string selectedExcursionType = selectedItem.ToString();
-
-                switch(selectedExcursionType) 
-                {
-                    case "обзорная":
-                        price = 1800 * Convert.ToInt32(upDownTourist.Text);
-                        break;
-
-                    case "историческая":
-                        price = 2400 * Convert.ToInt32(upDownTourist.Text);
-                        break;
-
-                    case "городская":
-                        price = 3000 * Convert.ToInt32(upDownTourist.Text);
-                        break;
-                    
-                    case "загородная":
-                        price = 3400 * Convert.ToInt32(upDownTourist.Text);
-                        break;
-
-                    case "музейная":
-                        price = 2000 * Convert.ToInt32(upDownTourist.Text);
-                        break;
-
-                    case "учебная":
-                        price = 800 * Convert.ToInt32(upDownTourist.Text);
-                        break;
-                }
-            }
-        }
+        #endregion
     }
 }
---
+++ TravelVoucher/Tools/ExcursionItem.cs
@@ -0,0 +1,16 @@
+﻿using Humanizer;
+using System;
+
+namespace TravelVoucher.Tools
+{
+    public class ExcursionItem
+    {
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double Price { get; set; }
+
+        public string CityImage { get; set; }
+    }
+}
