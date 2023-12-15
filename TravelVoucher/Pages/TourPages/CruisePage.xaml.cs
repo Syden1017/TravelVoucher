cdcb86218ed3c586a161b85b3131a4552d62afce
исп-32 <-32@LAB30-05>
2023-12-15 12:33:26 +0300

    ваиваиваиав

--- TravelVoucher/Pages/TourPages/CruisePage.xaml.cs
+++ TravelVoucher/Pages/TourPages/CruisePage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Humanizer;
+using System;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using TravelVoucher.Tools;
 using WpfApp1.Tools;
 
 namespace TravelVoucher.Pages.TourPages
@@ -84,7 +86,21 @@ namespace TravelVoucher.Pages.TourPages
 
         private void btnBuyTicket_Click(object sender, RoutedEventArgs e)
         {
+            // Получение данных из элементов управления
+            string selectedCity = cBoxRoute.SelectedItem.ToString();
+            DateTime selectedDate = datePickerCriuse.SelectedDate ?? DateTime.Now;
 
+            string name = GetCruiseName(selectedCity);
+            double calculatedPrice = CalculatePrice();
+
+            HolidayItem newItem = new HolidayItem
+            {
+                Name = name,
+                Date = selectedDate,
+                Price = calculatedPrice
+            };
+
+            lViewFoundCruise.Items.Add(newItem);
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
@@ -92,6 +108,31 @@ namespace TravelVoucher.Pages.TourPages
             Navigation.frmObj.GoBack();
         }
 
+        #region Методы
+        /// <summary>
+        /// Расчёт суммы в зависимости от типа экскурсии
+        /// </summary>
+        /// <returns>Подсчитанная стоимость поездки</returns>
+        private double CalculatePrice()
+        {
+            double price = 1200.00 * Convert.ToDouble(txtBoxCruiseDays.Text) * Convert.ToDouble(txtBoxNumberOfPeoples.Text);
+
+            return price;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="selectedCity">Выбранный элемент в combobox</param>
+        /// <returns></returns>
+        private string GetCruiseName(string selectedCity)
+        {
+            string excursionName = "Круиз до " + selectedCity.Humanize(LetterCasing.Title);
+            return excursionName;
+        }
+        #endregion
+
+        #region ToogleButton (для звёзд)
         private void ToggleButton_Click(object sender, RoutedEventArgs e)
         {
             ToggleButton button = sender as ToggleButton;
@@ -126,5 +167,6 @@ namespace TravelVoucher.Pages.TourPages
             int newvalue = int.Parse(button.Tag.ToString());
             int Value = newvalue;
         }
+        #endregion
     }
 }
