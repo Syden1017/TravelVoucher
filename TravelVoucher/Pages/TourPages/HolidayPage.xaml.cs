92edecf42deadccc8cf9502e91c512b2f1aa41f1
исп-32 <-32@LAB30-05>
2023-12-15 12:16:15 +0300

    fdgdfgfdgfdg

--- TravelVoucher/Pages/TourPages/HolidayPage.xaml.cs
+++ TravelVoucher/Pages/TourPages/HolidayPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Humanizer;
+using System;
 using System.Drawing;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
+using TravelVoucher.Tools;
 using WpfApp1.Tools;
 using Color = System.Windows.Media.Color;
 
@@ -15,36 +17,10 @@ namespace TravelVoucher.Pages.TourPages
     /// </summary>
     public partial class HolidayPage : Page
     {
-
         public partial class Rating : UserControl
         {
             public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Rating", typeof(int), typeof(Rating),
                 new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(RatingChanged)));
-
-            private int _max = 5;
-
-            public int Value
-            {
-                get
-                {
-                    return (int)GetValue(ValueProperty);
-                }
-                set
-                {
-                    if (value < 0)
-                    {
-                        SetValue(ValueProperty, 0);
-                    }
-                    else if (value > _max)
-                    {
-                        SetValue(ValueProperty, _max);
-                    }
-                    else
-                    {
-                        SetValue(ValueProperty, value);
-                    }
-                }
-            }
 
             private static void RatingChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
             {
@@ -58,14 +34,18 @@ namespace TravelVoucher.Pages.TourPages
                 {
                     button = childs[i] as ToggleButton;
                     if (button != null)
+                    {
                         button.IsChecked = true;
+                    }
                 }
 
                 for (int i = newval; i < childs.Count; i++)
                 {
                     button = childs[i] as ToggleButton;
                     if (button != null)
+                    {
                         button.IsChecked = false;
+                    }
                 }
 
             }
@@ -77,12 +57,14 @@ namespace TravelVoucher.Pages.TourPages
             InitializeComponent();
             string workingDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\"));
             string fileName = workingDirectory + @"txt-cities-russia.txt";
+            string file = workingDirectory + @"vehicleType.txt";
+            string foodFile = workingDirectory + @"food.txt";
 
             cBoxFrom.ItemsSource = File.ReadAllLines(fileName);
             cBoxTo.ItemsSource = File.ReadAllLines(fileName);
+            cBoxTransport.ItemsSource = File.ReadAllLines(file);
+            cBoxFood.ItemsSource = File.ReadAllLines(foodFile);
         }
-
-
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
@@ -91,18 +73,23 @@ namespace TravelVoucher.Pages.TourPages
 
         private void btnBuyTicket_Click(object sender, RoutedEventArgs e)
         {
-            Random rnd = new Random();
-            var Numb = rnd.Next(0, 1000000);
-            string Name = "Ваш билет" + $"Номер:{Numb}";
-            string Sity = $"Направление: {cBoxFrom.Text} -> {cBoxTo.Text}";
-            string Date = $"Дата - {DataPic.Text}";
-            string Transport = $"Вид транспорта - {cBoxTransport.Text}";
-            string Hotel = $"Отель - {cBoxHotel.Text}";
-            string Pitanie = $"Питание - {cBoxFood.Text}";
-            string ColHuman = $"Количество гостей - {txtBoxNumberOfGuests.Text}";
+            // Получение данных из элементов управления
+            string selectedCity = cBoxTo.SelectedItem.ToString();
+            DateTime selectedDate = datePickerHoliday.SelectedDate ?? DateTime.Now;
+            string vehicleType = cBoxTransport.SelectedItem.ToString();
 
+            string name = GetHolidayName(selectedCity);
+            double calculatedPrice = CalculatePrice(vehicleType);
 
-           
+            HolidayItem newItem = new HolidayItem
+            {
+                Name = name,
+                Date = selectedDate,
+                Price = calculatedPrice,
+                Vehicle = vehicleType
+            };
+
+            lViewFoundHoldays.Items.Add(newItem);
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
@@ -110,6 +97,63 @@ namespace TravelVoucher.Pages.TourPages
             Navigation.frmObj.GoBack();
         }
 
+        #region Методы
+        /// <summary>
+        /// Расчёт суммы в зависимости от типа экскурсии
+        /// </summary>
+        /// <returns>Подсчитанная стоимость поездки</returns>
+        private double CalculatePrice(string type)
+        {
+            double price = 0.00;
+
+            if (type != null)
+            {
+                string selectedVehicleType = type.ToString();
+
+                switch (selectedVehicleType)
+                {
+                    case "Машина (своя)":
+                        price = 1800.00 * Convert.ToDouble(upDownNumberOfGuests.Text);
+                        break;
+
+                    case "Машина (аренда)":
+                        price = 2400.00 * Convert.ToDouble(upDownNumberOfGuests.Text);
+                        break;
+
+                    case "Автобус":
+                        price = 3000.00 * Convert.ToDouble(upDownNumberOfGuests.Text);
+                        break;
+
+                    case "Поезд":
+                        price = 3400.00 * Convert.ToDouble(upDownNumberOfGuests.Text);
+                        break;
+
+                    case "Самолёт":
+                        price = 2000.00 * Convert.ToDouble(upDownNumberOfGuests.Text);
+                        break;
+
+                    case "Активные средства самостоятельного передвижения":
+                        price = 800.00 * Convert.ToDouble(upDownNumberOfGuests.Text);
+                        break;
+                }
+            }
+
+            return price;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="selectedCity">Выбранный элемент в combobox</param>
+        /// <returns></returns>
+        private string GetHolidayName(string selectedCity)
+        {
+            string excursionName = "Отдых в " + selectedCity.Humanize(LetterCasing.Title);
+            return excursionName;
+        }
+        #endregion
+
+        #region ToogleButton (для звёзд)
         private void ToggleButton_Click(object sender, RoutedEventArgs e)
         {
             ToggleButton button = sender as ToggleButton;
@@ -144,5 +188,6 @@ namespace TravelVoucher.Pages.TourPages
             int newvalue = int.Parse(button.Tag.ToString());
             int Value = newvalue;
         }
+        #endregion
     }
 }
